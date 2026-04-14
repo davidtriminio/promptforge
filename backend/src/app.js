7067f73b2a4dc47941538791a904309527f4ae9c
David Triminio <142177715+davidtriminio@users.noreply.github.com>
2026-04-13 20:56:19 -0600

    chore(backend): initial configuration

---
+++ backend/src/app.js
@@ -0,0 +1,14 @@
+﻿import express from "express"
+import cors from "cors"
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.status(200).json({
+        message: "PromptForge API is running."
+    })
+})
+
+export default app
---
+++ backend/src/server.js
@@ -0,0 +1,7 @@
+﻿import app from "./app.js";
+import {ENV} from "./config/ENV.js";
+const PORT = ENV.PORT
+
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`)
+})
