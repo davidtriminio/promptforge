d8248c3cc8ecdfd364bede8195301fdf58149a49
David Triminio <davidtriminio21@gmail.com>
2026-04-14 07:48:01 -0600

    refactor: add env variables validation in ENV.js

--- backend/src/config/ENV.js
+++ backend/src/config/ENV.js
@@ -4,8 +4,17 @@ dotenv.config({
     quiet: true
 })
 
+function getENV(name) {
+    const value = process.env[name]
+
+    if(!value){
+        throw new Error(`Environment variable ${name} is required`)
+    }
+    return value
+}
+
 export const ENV = {
-    PORT: process.env.PORT || 5000,
-    NODE_ENV: process.env.NODE_ENV,
-    MONGO_URI: process.env.MONGO_URI
+    PORT: getENV("PORT") || 5000,
+    NODE_ENV: getENV("NODE_ENV"),
+    MONGO_URI: getENV("MONGO_URI")
 }
