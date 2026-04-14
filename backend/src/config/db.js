9473488929a49f2369f8d8a9aea4d611a5038767
David Triminio <142177715+davidtriminio@users.noreply.github.com>
2026-04-14 07:48:01 -0600

    refactor: add env variables validation in ENV.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -4,10 +4,6 @@ import {ENV} from "./ENV.js";
 export const connectDB = async () => {
     try {
         const mongoUri = ENV.MONGO_URI
-        if (!mongoUri) {
-            throw new Error("MONGO_URI is not defined in environment variables.")
-        }
-
         const connection = await mongoose.connect(mongoUri)
         console.log(`MongoDB connected: ${connection.connection.host}`)
 
