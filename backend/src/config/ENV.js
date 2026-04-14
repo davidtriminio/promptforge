735083aef1640a6c151a47026cc614db9019069f
David Triminio <davidtriminio21@gmail.com>
2026-04-13 20:57:38 -0600

    chore(env): add MONGO URI env variable

--- backend/src/config/ENV.js
+++ backend/src/config/ENV.js
@@ -6,5 +6,6 @@ dotenv.config({
 
 export const ENV = {
     PORT: process.env.PORT || 5000,
-    NODE_ENV: process.env.NODE_ENV
+    NODE_ENV: process.env.NODE_ENV,
+    MONGO_URI: process.env.MONGO_URI
 }
