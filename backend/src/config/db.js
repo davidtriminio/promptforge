bf59cd849b51968c81c8506f37b08f10170edc78
David Triminio <davidtriminio21@gmail.com>
2026-04-13 21:03:37 -0600

    chore(server): set mongodb connection

---
+++ backend/src/config/db.js
@@ -0,0 +1,18 @@
+﻿import mongoose from "mongoose";
+import {ENV} from "./ENV.js";
+
+export const dbConnect = async () => {
+    try {
+        const mongoUri = ENV.MONGO_URI
+        if (!mongoUri) {
+            throw new Error("MONGO_URI is not defined in environment variables.")
+        }
+
+        const connection = await mongoose.connect(mongoUri)
+        console.log(`MongoDB connected: ${connection.connection.host}`)
+
+    } catch (e) {
+        console.error(`MongoDB connection error: ${e.message}`)
+        process.exit(1)
+    }
+}
