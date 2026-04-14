3367cbe6da21c35cff1a9452bdb71296a5699bed
David Triminio <davidtriminio21@gmail.com>
2026-04-14 07:06:37 -0600

    feat(backend): add and set user model

---
+++ backend/src/models/User.model.js
@@ -0,0 +1,38 @@
+﻿import mongoose, {mongo} from "mongoose";
+
+const userSchema = new mongoose.Schema({
+        name: {
+            type: String,
+            required: [true, "Name is required."],
+            trim: true,
+            minLength: [2, "Name must be at least 2 characters."],
+            maxLength: [50, "Name cannot exceed 50 characters."],
+        },
+        email: {
+            type: String,
+            required: [true, "Email is required."],
+            unique: true,
+            lowercase: true,
+            trim: true,
+            match: [
+                /^\S+@\S+\.\S+$/,
+                "Please provide a valid email address",
+            ]
+        },
+        password: {
+            type: String,
+            required: [true, "Password is required."],
+            minLength: [6, "Password must be at least 6 characters."],
+            select: false
+        },
+        role: {
+            type: String,
+            enum: ["user", "admin"],
+            default: "user"
+        }
+    },
+    {timestamps: true}
+)
+
+const User = mongoose.model("User", userSchema)
+export default User
