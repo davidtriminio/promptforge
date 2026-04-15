76c6759b07ff74c4bf0c0a0a7ab0b54ccdc17eee
David Triminio <142177715+davidtriminio@users.noreply.github.com>
2026-04-14 20:01:28 -0600

    feat(backend): add util for generateToken

---
+++ backend/src/utils/generateToken.js
@@ -0,0 +1,14 @@
+﻿import jwt from "jsonwebtoken"
+import {ENV} from "../config/ENV.js";
+
+const generateToken = (userId) => {
+    jwt.sign(
+        {id: userId},
+        ENV.JWT_SECRET,
+        {
+            expiresIn: ENV.JWT_EXPIRES_IN || "7d"
+        }
+    )
+}
+
+export default generateToken
