882f16f687336b8958d8f59476b6fb10aae350fa
David Triminio <142177715+davidtriminio@users.noreply.github.com>
2026-04-14 21:34:19 -0600

    fix(backend): select password in auth controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -37,7 +37,7 @@ export const register = async (req, res) => {
 
 export const login = async (req, res) => {
     const {email, password} = req.body
-    const user = await User.findOne({email})
+    const user = await User.findOne({email}).select("+password")
 
     if (!user) {
         return res.status(401).json({
