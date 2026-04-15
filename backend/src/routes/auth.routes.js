6dfe6f5f1b7f0a0cd04acbf23c6d6a8b7e9a5ba4
David Triminio <142177715+davidtriminio@users.noreply.github.com>
2026-04-14 21:17:30 -0600

    feat(backend): implement auth routes

---
+++ backend/src/routes/auth.routes.js
@@ -0,0 +1,11 @@
+﻿import {Router} from "express";
+import {loginValidator, registerValidator} from "../middlewares/auth.validator.js";
+import validate from "../middlewares/validate.middleware.js";
+import {login, register} from "../controllers/auth.controller.js";
+
+const router = Router()
+
+router.post("/register", registerValidator, validate, register)
+router.post("/login", loginValidator, validate, login)
+
+export default router
