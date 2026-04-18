f0956d57d15c603f885a6168beecece23c017f67
David Triminio <davidtriminio21@gmail.com>
2026-04-17 19:46:38 -0600

    feat(backend): add prompt schema with validations

---
+++ backend/src/models/Prompt.model.js
@@ -0,0 +1,45 @@
+﻿import mongoose from "mongoose";
+
+const promptSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+        index: true
+    },
+    title: {
+        type: String,
+        required: [true, "Title is required."],
+        trim: true,
+        minLength: [3, "Title must be at least 3 characters."],
+        maxLength: [100, "Title cannot exceed 100 characters."]
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: [300, "Description cannot exceed 300 characters."],
+        default: ""
+    },
+    content:{
+        type: String,
+        required: [true, "Prompt is required."],
+        trim: true,
+        minLength: [10, "Prompt content must be at least 10 characters."],
+        maxLength: [10000, "Prompt cannot exceed 10000 characters."]
+    },
+    tags: {
+        type: [String],
+        default: []
+    },
+    isFavorite: {
+        type: Boolean,
+        default: false
+    }
+})
+
+promptSchema.index({user: 1, createdAt: -1})
+
+export const Prompt = mongoose.model("Prompst", promptSchema)
+
+export default Prompt
