177ae22cb37f3d1ef8207f99cd169030b14b6edb
David Triminio <142177715+davidtriminio@users.noreply.github.com>
2026-04-17 21:50:55 -0600

    feat(backend): implement prompt routes

---
+++ backend/src/routes/prompt.routes.js
@@ -0,0 +1,30 @@
+﻿import express from "express";
+import protect from "../middlewares/auth.middleware.js";
+import {
+    createPrompt,
+    getPromptById,
+    getPrompts,
+    toggleFavoritePrompt,
+    updatePrompt
+} from "../controllers/prompt.controller.js";
+import {createPromptValidator, updatePromptValidator} from "../validators/prompt.validator.js";
+import validate from "../middlewares/validate.middleware.js";
+
+const router = express.Router()
+
+router.use(protect)
+
+router
+.route("/")
+.get(getPrompts)
+.post(createPromptValidator, validate, createPrompt)
+
+router
+.route("/:id")
+.get(getPromptById)
+.put(updatePromptValidator, validate, updatePrompt)
+
+router
+.patch("/:id/favorite", toggleFavoritePrompt)
+
+export default router
