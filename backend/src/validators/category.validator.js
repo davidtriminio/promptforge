b8192313d347e79ab87e89729c381c0c97fb4981
David Triminio <142177715+davidtriminio@users.noreply.github.com>
2026-04-18 23:33:15 -0600

    feat(backend): add category validator

---
+++ backend/src/validators/category.validator.js
@@ -0,0 +1,30 @@
+﻿import {body} from "express-validator";
+
+export const createCategoryValidator = [
+    body("name")
+    .trim()
+    .notEmpty()
+    .withMessage("Category name is required.")
+    .isLength({min: 2, max: 40})
+    .withMessage("Category name must be between 2 and 40 characters."),
+
+    body("color")
+    .optional()
+    .trim()
+    .matches(/^#([0-9A-F]{3}){1,2}$/i)
+    .withMessage("Color must be a valid hex color.")
+]
+
+export const updateCategoryValidator = [
+    body("name")
+    .optional()
+    .trim()
+    .isLength({min: 2, max: 40})
+    .withMessage("Category name must be between 2 and 40 characters."),
+
+    body("color")
+    .optional()
+    .trim()
+    .matches(/^#([0-9A-F]{3}){1,2}$/i)
+    .withMessage("Color must be a valid hex color.")
+]
