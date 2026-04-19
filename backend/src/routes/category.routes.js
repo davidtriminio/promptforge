36a881ec676e3f370643c8ba2e24c482cb1eb710
David Triminio <142177715+davidtriminio@users.noreply.github.com>
2026-04-19 17:41:43 -0600

    feat(backend): implement category routes

---
+++ backend/src/routes/category.routes.js
@@ -0,0 +1,18 @@
+﻿import express from "express";
+import {createCategory, deleteCategory, getCategories, updateCategory} from "../controllers/category.controller.js";
+import {createCategoryValidator, updateCategoryValidator} from "../validators/category.validator.js";
+import validate from "../middlewares/validate.middleware.js";
+
+const router = express.Router()
+
+router
+.route("/")
+.get(getCategories)
+.post(createCategoryValidator, validate, createCategory)
+
+router
+.route("/:id")
+.put(updateCategoryValidator, validate, updateCategory)
+.delete(deleteCategory)
+
+export default router
