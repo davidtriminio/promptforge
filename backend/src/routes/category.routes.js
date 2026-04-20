6329e937ac1f507c5f96abd5869b1204844721ee
David Triminio <davidtriminio21@gmail.com>
2026-04-19 22:52:57 -0600

    feat(backend): add route protection to category

--- backend/src/routes/category.routes.js
+++ backend/src/routes/category.routes.js
@@ -2,8 +2,10 @@
 import {createCategory, deleteCategory, getCategories, updateCategory} from "../controllers/category.controller.js";
 import {createCategoryValidator, updateCategoryValidator} from "../validators/category.validator.js";
 import validate from "../middlewares/validate.middleware.js";
+import protect from "../middlewares/auth.middleware.js";
 
 const router = express.Router()
+router.use(protect)
 
 router
 .route("/")
