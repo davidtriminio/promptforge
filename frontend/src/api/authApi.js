df6dfeb6116868c1859b5baa365c552467345c96
David Triminio <davidtriminio21@gmail.com>
2026-04-21 20:53:25 -0600

    feat(frontend): add and set authApi

---
+++ frontend/src/api/authApi.js
@@ -0,0 +1,16 @@
+﻿import axiosClient from "./axiosClient.js";
+
+export const registerRequest = async (payload) => {
+    const {data} = await axiosClient.post("/auth/register", payload)
+    return data
+}
+
+export const loginRequest = async (payload) => {
+    const {data} =await axiosClient.post("/auth/login", payload)
+    return data
+}
+
+export const meRequest = async () => {
+    const {data} = await  axiosClient.post("auth/me")
+    return data
+}
