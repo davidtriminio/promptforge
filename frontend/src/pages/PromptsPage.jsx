8d0af4546e39d08cb55a0129de2deefa02918f4a
David Triminio <142177715+davidtriminio@users.noreply.github.com>
2026-04-21 22:09:47 -0600

    feat(frontend): add PromptsPage base

---
+++ frontend/src/pages/PromptsPage.jsx
@@ -0,0 +1,11 @@
+﻿import React from 'react'
+
+const PromptsPage = () => {
+    return (
+        <div>
+            <h1>Prompts</h1>
+            <p className="mt-2 font-semibold">Here you will manage your prompt library.</p>
+        </div>
+    )
+}
+export default PromptsPage
