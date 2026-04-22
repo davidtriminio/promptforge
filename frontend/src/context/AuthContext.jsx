3fcc35282d679cedce770db50eec36838488dbbd
David Triminio <142177715+davidtriminio@users.noreply.github.com>
2026-04-21 21:14:56 -0600

    feat(frontend): add and set AuthContext

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,74 @@
+﻿import {createContext, useEffect, useMemo, useState} from "react";
+import {loginRequest, meRequest, registerRequest} from "../api/authApi.js";
+import {storage} from "../utils/storage.js";
+
+export const AuthContext = createContext(null)
+
+export const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null)
+    const [authLoading, setAuthLoading] = useState(true)
+
+    const register = async (formData) => {
+        const data = await registerRequest(formData)
+
+        storage.setToken(data.token)
+        setUser(data.user)
+
+        return data
+    }
+
+    const login = async (formData) => {
+        const data = await loginRequest(formData)
+
+        storage.setToken(data.token)
+        setUser(data.user)
+
+        return data
+    }
+
+    const logout = async () => {
+        storage.removeToken()
+        setUser(null)
+    }
+
+    const loadAuthenticatedUser = async () => {
+        const token = storage.getToken()
+
+        if (!token) {
+            setAuthLoading(false)
+            return
+        }
+
+        try {
+            const data = await meRequest()
+            setUser(data.user)
+        } catch (e) {
+            storage.removeToken()
+            setUser(null)
+        } finally {
+            setAuthLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        return () => {
+            loadAuthenticatedUser()
+        };
+    }, [[]])
+
+    const value = useMemo(() => ({
+        user,
+        authLoading,
+        isAuthenticated: Boolean(user),
+        register,
+        login,
+        logout,
+        setUser
+    }), [user, authLoading])
+
+    return (
+        <AuthContext.Provider value={value}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
