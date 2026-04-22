e82d0651d365a69e2f112632fceae9673058de90
David Triminio <davidtriminio21@gmail.com>
2026-04-21 20:32:23 -0600

    chore(frontend): add initial ENV variables

---
+++ frontend/src/utils/ENV.js
@@ -0,0 +1,13 @@
+﻿function getENV(name){
+    const value = import.meta.env[name]
+
+    if(!value){
+        throw new Error(`Environment variables ${name} is required`)
+    }
+
+    return value
+}
+
+export const ENV = {
+    VITE_API_URL: getENV("VITE_API_URL")
+}
