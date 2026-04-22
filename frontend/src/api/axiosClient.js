a462f05eba47f1025c198247bc7574ffbce5464e
David Triminio <davidtriminio21@gmail.com>
2026-04-21 20:32:50 -0600

    feat(frontend): add axiosClient config

---
+++ frontend/src/api/axiosClient.js
@@ -0,0 +1,18 @@
+﻿import axios from "axios";
+import {ENV} from "../utils/ENV.js";
+
+const axiosClient = axios.create({
+    baseURL: ENV.VITE_API_URL
+})
+
+axiosClient.interceptors.request.use((config) => {
+    const token = localStorage.getItem("promptforge_token")
+
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`
+    }
+
+    return config
+})
+
+export default axiosClient
