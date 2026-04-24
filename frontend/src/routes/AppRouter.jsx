c73a20ef0ce905bd3d29ff7480eb0e0ab189d368
David Triminio <davidtriminio21@gmail.com>
2026-04-23 18:51:12 -0600

    feat(frontend): add app routes

---
+++ frontend/src/routes/AppRouter.jsx
@@ -0,0 +1,34 @@
+﻿import React from 'react'
+import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
+import GuestRoute from "./GuestRoute.jsx";
+import LoginPage from "../pages/LoginPage.jsx";
+import RegisterPage from "../pages/RegisterPage.jsx";
+import ProtectedRoute from "./ProtectedRoute.jsx";
+import AppLayout from "../components/layout/AppLayout.jsx";
+import DashboardPage from "../pages/DashboardPage.jsx";
+import PromptsPage from "../pages/PromptsPage.jsx";
+
+const AppRouter = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route element={GuestRoute}>
+                    <Route path="/login" element={LoginPage}/>
+                    <Route path="/register" element={RegisterPage}/>
+                </Route>
+
+                <Route element={ProtectedRoute}>
+                    <Route element={AppLayout}/>
+                    <Route path="/dashboard" element={DashboardPage}/>
+                    <Route path="/prompts" element={PromptsPage}/>
+                </Route>
+
+                <Route path="/" element={<Navigate to="/dashboard" replace/>}/>
+                <Route path="*" element={<Navigate to="/dashboard" replace/>}/>
+            </Routes>
+
+
+        </BrowserRouter>
+    )
+}
+export default AppRouter
