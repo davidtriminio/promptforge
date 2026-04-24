341d64c5a59fd12ca3579fe043f56ee95ef95f9a
David Triminio <davidtriminio21@gmail.com>
2026-04-23 20:36:53 -0600

    fix(frontend): add dynamic import for axiosClient

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -1,16 +1,20 @@
-﻿import axiosClient from "./axiosClient.js";
-
-export const registerRequest = async (payload) => {
+﻿export const registerRequest = async (payload) => {
+    const { default: getAxiosClient } = await import("./axiosClient.js")
+    const axiosClient = getAxiosClient()
     const {data} = await axiosClient.post("/auth/register", payload)
     return data
 }
 
 export const loginRequest = async (payload) => {
+    const { default: getAxiosClient } = await import("./axiosClient.js")
+    const axiosClient = getAxiosClient()
     const {data} =await axiosClient.post("/auth/login", payload)
     return data
 }
 
 export const meRequest = async () => {
-    const {data} = await  axiosClient.post("auth/me")
+    const { default: getAxiosClient } = await import("./axiosClient.js")
+    const axiosClient = getAxiosClient()
+    const {data} = await  axiosClient.post("/auth/me")
     return data
 }
