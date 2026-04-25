1bea94168f763d885729bd071de1e134d46f70b5
David Triminio <142177715+davidtriminio@users.noreply.github.com>
2026-04-25 16:31:58 -0600

    feat(frontend): add dashboardApi

---
+++ frontend/src/api/dashboardApi.js
@@ -0,0 +1,10 @@
+﻿import axiosClient from "./axiosClient.js";
+
+export const dashboardApi = async () => {
+    try {
+        const {data} = await axiosClient.get("/dashboard/stats")
+        return data
+    } catch (e) {
+        throw e
+    }
+}
