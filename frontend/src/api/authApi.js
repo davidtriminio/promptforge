ccc55497c092f1c38fa60881c21398f5b0bbc959
David Triminio <davidtriminio21@gmail.com>
2026-04-24 19:38:47 -0600

    fix(frontend): load env before creating axios init

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -1,20 +1,20 @@
 ﻿export const registerRequest = async (payload) => {
     const { default: getAxiosClient } = await import("./axiosClient.js")
-    const axiosClient = getAxiosClient()
+    const axiosClient = await getAxiosClient()
     const {data} = await axiosClient.post("/auth/register", payload)
     return data
 }
 
 export const loginRequest = async (payload) => {
     const { default: getAxiosClient } = await import("./axiosClient.js")
-    const axiosClient = getAxiosClient()
+    const axiosClient = await getAxiosClient()
     const {data} =await axiosClient.post("/auth/login", payload)
     return data
 }
 
 export const meRequest = async () => {
     const { default: getAxiosClient } = await import("./axiosClient.js")
-    const axiosClient = getAxiosClient()
+    const axiosClient = await getAxiosClient()
     const {data} = await  axiosClient.post("/auth/me")
     return data
 }
--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,10 +1,10 @@
 ﻿import axios from "axios";
-import {ENV} from "../utils/ENV.js";
 
 let axiosClientInstance = null;
 
-function getAxiosClient() {
+async function getAxiosClient() {
     if (!axiosClientInstance) {
+        const {ENV} = await import("../utils/ENV.js");
         axiosClientInstance = axios.create({
             baseURL: ENV.VITE_API_URL
         })
