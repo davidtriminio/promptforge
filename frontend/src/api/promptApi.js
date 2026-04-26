88a50a692fd16bcd3677eb1c552e12238d76ac0b
David Triminio <davidtriminio21@gmail.com>
2026-04-26 09:14:57 -0600

    feat(frontend): add promptApi

---
+++ frontend/src/api/promptApi.js
@@ -0,0 +1,28 @@
+﻿import getAxiosClient from "./axiosClient.js";
+
+const axiosClient = getAxiosClient()
+
+export const getPromptRequest = async (params = {}) => {
+    const {data} = await axiosClient.get("/prompts", {params})
+    return data
+}
+
+export const createPromptRequest = async (payload) => {
+    const {data} = await axiosClient.post("/prompts", payload)
+    return data
+}
+
+export const updatePromptRequest = async (id, payload) => {
+    const {data} = await axiosClient.put(`/prompts/${id}`, payload)
+    return data
+}
+
+export const deletePromptRequest = async (id) => {
+    const {data} = await axiosClient.delete(`/prompts/${id}`)
+    return data
+}
+
+export const toggleFavoritePrompt = async (id) => {
+    const {data} = await axiosClient.patch(`/prompts/${id}`)
+    return data
+}
