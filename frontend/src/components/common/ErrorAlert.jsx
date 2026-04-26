face382dc74a753b48fbb31e09a54a8636a924c3
David Triminio <davidtriminio21@gmail.com>
2026-04-26 09:28:03 -0600

    feat(frontend): add ErrorAlert component

---
+++ frontend/src/components/common/ErrorAlert.jsx
@@ -0,0 +1,8 @@
+﻿const ErrorAlert = ({message = "Something went wrong."}) => {
+    return (
+        <div className={"alert alert-error rounded-lg"}>
+            <span>{message}</span>
+        </div>
+    )
+}
+export default ErrorAlert
