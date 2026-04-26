c84c059e94d3ab31ad1906c105ac7a5490c001e0
David Triminio <142177715+davidtriminio@users.noreply.github.com>
2026-04-26 09:20:36 -0600

    feat(fronend): add categoryApi

---
+++ frontend/src/api/categoryApi.js
@@ -0,0 +1,22 @@
+﻿import getAxiosClient from "./axiosClient.js";
+
+const axiosClient = getAxiosClient()
+
+export const getCategoriesRequest = async() => {
+    const {data} = await axiosClient.get("/categories")
+    return data
+}
+
+export const createCategoryRequest = async(payload) =>{
+    const {data} = await axiosClient.post("/categories", payload)
+    return data
+}
+
+export const updateCategoryRequest = async(id, payload) => {
+    const {data} = await axiosClient.put(`/categories/${id}`, payload)
+    return data
+}
+
+export const deleteCategoryRequest = async (id) => {
+    const {data} = await axiosClient.delete(`/prompts/${id}`)
+}
