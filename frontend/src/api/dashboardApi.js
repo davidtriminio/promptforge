d9d5b4ec84b0c64ae361bf42474fbbb7497a0f31
David Triminio <142177715+davidtriminio@users.noreply.github.com>
2026-04-25 22:44:37 -0600

    fix(frontend): correct axiosClient usage

--- frontend/src/api/dashboardApi.js
+++ frontend/src/api/dashboardApi.js
@@ -1,7 +1,8 @@
-﻿import axiosClient from "./axiosClient.js";
+﻿import getAxiosClient from "./axiosClient.js";
 
 export const getDashboardStatsRequest = async () => {
     try {
+        const axiosClient = await getAxiosClient();
         const {data} = await axiosClient.get("/dashboard/stats")
         return data
     } catch (e) {
