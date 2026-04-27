23d37db63baa270d1b7a7ca9bd6d42a001f5abc0
David Triminio <142177715+davidtriminio@users.noreply.github.com>
2026-04-27 07:23:09 -0600

    fix(frontend): improve getAxiosClient call

--- frontend/src/api/categoryApi.js
+++ frontend/src/api/categoryApi.js
@@ -1,22 +1,25 @@
 ﻿import getAxiosClient from "./axiosClient.js";
 
-const axiosClient = getAxiosClient()
-
 export const getCategoriesRequest = async() => {
+    const axiosClient = await getAxiosClient()
     const {data} = await axiosClient.get("/categories")
     return data
 }
 
 export const createCategoryRequest = async(payload) =>{
+    const axiosClient = await getAxiosClient()
     const {data} = await axiosClient.post("/categories", payload)
     return data
 }
 
 export const updateCategoryRequest = async(id, payload) => {
+    const axiosClient = await getAxiosClient()
     const {data} = await axiosClient.put(`/categories/${id}`, payload)
     return data
 }
 
 export const deleteCategoryRequest = async (id) => {
-    const {data} = await axiosClient.delete(`/prompts/${id}`)
+    const axiosClient = await getAxiosClient()
+    const {data} = await axiosClient.delete(`/categories/${id}`)
+    return data
 }
