cb6da389836e4a2edefff0a69479772684845d53
David Triminio <142177715+davidtriminio@users.noreply.github.com>
2026-04-28 22:00:32 -0600

    feat(frontend): enhance ErrorAlert UI

--- frontend/src/components/common/ErrorAlert.jsx
+++ frontend/src/components/common/ErrorAlert.jsx
@@ -1,8 +1,13 @@
-﻿const ErrorAlert = ({message = "Something went wrong."}) => {
+﻿import {Alert, AlertDescription, AlertTitle} from "@/components/ui/alert.tsx";
+import {Icon} from "@iconify/react";
+
+const ErrorAlert = ({message = "Algo salió Mal."}) => {
     return (
-        <div className={"alert alert-error rounded-lg"}>
-            <span>{message}</span>
-        </div>
+        <Alert variant={"destructive"}>
+            <Icon icon={"solar:danger-triangle-bold-duotone"} className={"h-4 w-4"}/>
+            <AlertTitle>Error</AlertTitle>
+            <AlertDescription>{message}</AlertDescription>
+        </Alert>
     )
 }
 export default ErrorAlert
