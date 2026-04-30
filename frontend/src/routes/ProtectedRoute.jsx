e59434c4c8bce36b5cbf6f1bfe97b725c4f0955c
David Triminio <davidtriminio21@gmail.com>
2026-04-29 22:21:17 -0600

    feat(frontend): improve Loading and Empty states

--- frontend/src/routes/ProtectedRoute.jsx
+++ frontend/src/routes/ProtectedRoute.jsx
@@ -1,12 +1,13 @@
 ﻿import React from 'react'
 import {useAuth} from "../hooks/useAuth.js";
 import {Navigate, Outlet} from "react-router-dom";
+import LoadingState from "@/components/common/LoadingState.jsx";
 
 const ProtectedRoute = () => {
     const {isAuthenticated, authLoading} = useAuth()
 
     if (authLoading) {
-        return <p>Loading...</p>
+        return <LoadingState variant={"fullscreen"} message={"Validando sesión..."}/>
     }
 
     if (!isAuthenticated) {
