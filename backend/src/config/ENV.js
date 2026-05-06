c8e6cc839585c21f935e6e59f5cc0d4ee76c9a29
David Triminio <davidtriminio21@gmail.com>
2026-05-06 07:36:59 -0600

    chore(backend): prep render deploy

--- backend/src/config/ENV.js
+++ backend/src/config/ENV.js
@@ -4,19 +4,23 @@ dotenv.config({
     quiet: true
 })
 
-function getENV(name) {
+function getENV(name, defaultValue = undefined) {
     const value = process.env[name]
 
-    if(!value){
-        throw new Error(`Environment variable ${name} is required`)
+    if(value === undefined || value === ""){
+        if (defaultValue === undefined) {
+            throw new Error(`Environment variable ${name} is required`)
+        }
+        return defaultValue
     }
     return value
 }
 
 export const ENV = {
-    PORT: getENV("PORT") || 5000,
-    NODE_ENV: getENV("NODE_ENV"),
+    PORT: Number(getENV("PORT",5000)),
+    NODE_ENV: getENV("NODE_ENV", "development"),
     MONGO_URI: getENV("MONGO_URI"),
     JWT_SECRET: getENV("JWT_SECRET"),
-    JWT_EXPIRES_IN: getENV("JWT_EXPIRES_IN")
+    JWT_EXPIRES_IN: getENV("JWT_EXPIRES_IN", "7d"),
+    CORS_ORIGINS: getENV("CORS_ORIGINS", "http://localhost:5173")
 }
